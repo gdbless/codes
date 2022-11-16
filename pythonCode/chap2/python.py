9d6ec2e3f65ecec61560a89650b2e3c93c964090
marktwainme <a1264879714@126.com>
2022-11-16 22:11:42 +0800

    02

--- pythonCode/chap2/python.py
+++ pythonCode/chap2/python.py
@@ -8,29 +8,26 @@ def read_stl_file(filename):
 
 
 def open_vertex_code(lines):
-    vermats = []  # 建立所有点的数组
+    vermats = [[] for i in range(3)] # 建立所有点的数组
     for line in lines:
         if line.startswith('vertex'):
-            temp = line.strip('vertex').strip()  # 保留删除 vertex 字样后的每一行数据（三个浮点数据字符串）
-            temp = list(map(float, vermat))  # 转化为浮点数保存为数组
-            vermats.append(temp)
+            temp = line.strip('vertex\n ').split(' ')  # 保留删除 vertex 字样后的每一行数据（三个浮点数据字符串）
+            temp = list(map(float, temp))  # 转化为浮点数保存为数组
+            vermats[0].append(temp[0])
+            vermats[1].append(temp[2])
+            vermats[2].append(temp[2])
     return vermats
 
 
-def construct_topology(vermats):
-    pgon = []
-    for vermat in vermats[::3]:
-        pgon.append(vermat)
-    return pgon
-
-
-def show_pgon(pgon):
+def show_pgon(vermats):
     ax = plt.figure().add_subplot(projection='3d')
-    ax.plot_trisurf(pgon[0], pgon[1], pgon[2], linewidth=0.2, antialiased=True)
+    ax.plot_trisurf(vermats[0], vermats[1], vermats[2], linewidth=0.2, antialiased=True)
+    plt.savefig('tri_surf.png')
     plt.show()
 
 
-if __name__ == __main__:
+
+if __name__ == '__main__':
     filename = 'model1or.stl'
     vermats = open_vertex_code(read_stl_file(filename))
-    show_pgon(construct_topology(vermats))
+    show_pgon(vermats)
